75afb030ea547014c6fce468c3abd9e9e309c524
Ed Wu <s784533peter@livemail.tw>
2018-10-22 22:05:25 +0800

    change to while >0

--- password.py
+++ password.py
@@ -1,12 +1,14 @@
 password = 'a123'
 i = 3
-while True:
-     pwd = input('請輸入密碼')
-     if pwd == password:
-         print('登入成功！')
-         break
-     else:
-     	i = i - 1
-     	print('密碼錯誤，請重新輸入','還有',i,'次機會')
-     	if i == 0:
-     	    break
+while i > 0:
+    i = i - 1
+    pwd = input('請輸入密碼: ')
+    if pwd == password:
+        print('登入成功！')
+        break
+    else:
+        print('密碼錯誤')
+        if i > 0:
+     	    print('還有',i,'次機會')
+        else:
+     	    print('請過三十分鐘後再試')
